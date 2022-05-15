d81c0286140d56dc67c70c83513e54079edc754b
ruphert <ruphert6@gmail.com>
2022-05-15 23:37:47 +0800

    created feerbackItem, feedbackList, Card components + data file

--- feedbacker/src/App.js
+++ feedbacker/src/App.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import feedbackData from "./data/FeedbackData";
 
 function App() {
+  const [feedback, setFeedback] = useState(feedbackData);
   return (
     <>
       <Header />
       <div className="container">
-        <h1>My App</h1>
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   );
---
+++ feedbacker/src/data/FeedbackData.js
@@ -0,0 +1,34 @@
+const feedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem fugiat totam id sed ipsa saepe omnis facere voluptates neque exercitationem.",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem fugiat totam id sed ipsa saepe omnis facere voluptates neque exercitationem.",
+  },
+  {
+    id: 3,
+    rating: 6,
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem fugiat totam id sed ipsa saepe omnis facere voluptates neque exercitationem.",
+  },
+  {
+    id: 4,
+    rating: 10,
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem fugiat totam id sed ipsa saepe omnis facere voluptates neque exercitationem.",
+  },
+  {
+    id: 5,
+    rating: 8,
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem fugiat totam id sed ipsa saepe omnis facere voluptates neque exercitationem.",
+  },
+  {
+    id: 6,
+    rating: 3,
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem fugiat totam id sed ipsa saepe omnis facere voluptates neque exercitationem.",
+  },
+];
+
+export default feedbackData;
