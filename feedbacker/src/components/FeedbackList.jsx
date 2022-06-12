3c88480a8cdbf59f991f49a7f4d8343765aebc48
ruphert <ruphert6@gmail.com>
2022-06-12 15:50:14 +0800

    add package.json modification

--- feedbacker/src/components/FeedbackList.jsx
+++ feedbacker/src/components/FeedbackList.jsx
@@ -1,8 +1,11 @@
-import FeedbackItem from "./FeedbackItem";
-import { useContext } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import FeedbackContext from "../context/FeedbackContext";
+import { useContext } from "react";
+import FeedbackItem from "./FeedbackItem";
 import Spinner from "./shared/Spinner";
+import FeedbackContext from "../context/FeedbackContext";
+
+// NOTE: added layout prop for nicer animation
+// https://www.framer.com/docs/animation/#layout-animations
 
 function FeedbackList() {
   const { feedback, isLoading } = useContext(FeedbackContext);
@@ -22,6 +25,7 @@ function FeedbackList() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
+            layout
           >
             <FeedbackItem key={item.id} item={item} />
           </motion.div>
@@ -29,6 +33,14 @@ function FeedbackList() {
       </AnimatePresence>
     </div>
   );
+
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // )
 }
 
 export default FeedbackList;
